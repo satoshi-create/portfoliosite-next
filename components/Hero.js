3ec8dc413c3c1c61d1b16ee2d275e54dfcae5063
satoshi <mimizunobouken@gmail.com>
2022-08-26 03:42:05 +0900

    Heroコンポーネントをレスポンシブに修正

--- components/Hero.js
+++ components/Hero.js
@@ -5,34 +5,35 @@ import socialLinks from "../libs/socialLinks";
 const Hero = () => {
   return (
     <section className="parts-grid">
-      <div className={styles.hero}>
-        <div className={styles.box}>
-          <div className={styles.title}>
-            <h1>縁</h1>
-            <h1>側</h1>
-            <h1>工</h1>
-            <h1>房</h1>
-            <div className={styles.engawaline}>
-              <div className={`${styles.line} ${styles.yoko}`}></div>
-              <div className={`${styles.line} ${styles.tate}`}></div>
-            </div>
+      <div className={styles.box}>
+        <figure className={styles.heroFigure}>
+          <img src="/hero.png" className={styles.heroImg} />
+        </figure>
+        <div className={styles.title}>
+          <h1>縁</h1>
+          <h1>側</h1>
+          <h1>工</h1>
+          <h1>房</h1>
+          <div className={styles.engawaline}>
+            <div className={`${styles.line} ${styles.yoko}`}></div>
+            <div className={`${styles.line} ${styles.tate}`}></div>
           </div>
-          <h2 className={`${styles.catch} ${styles.catch1}`}>MARGINAL</h2>
-          <h2 className={`${styles.catch} ${styles.catch2}`}>DESIGN</h2>
-          <ul className={styles.links}>
-            {socialLinks.map((item, index) => {
-              const { name, icon, path } = item;
-              return (
-                <li key={index}>
-                  <socialLinks href={path}>
-                    <a className={styles.icon}>{icon}</a>
-                  </socialLinks>
-                </li>
-              );
-            })}
-          </ul>
-          <button className={`btn ${styles.herobtn}`}>作品を見る</button>
         </div>
+        <h2 className={`${styles.catch} ${styles.catch1}`}>MARGINAL</h2>
+        <h2 className={`${styles.catch} ${styles.catch2}`}>DESIGN</h2>
+        <ul className={styles.links}>
+          {socialLinks.map((item, index) => {
+            const { name, icon, path } = item;
+            return (
+              <li key={index}>
+                <socialLinks href={path}>
+                  <a className={styles.icon}>{icon}</a>
+                </socialLinks>
+              </li>
+            );
+          })}
+        </ul>
+        <button className={`btn ${styles.herobtn}`}>作品を見る</button>
       </div>
     </section>
   );

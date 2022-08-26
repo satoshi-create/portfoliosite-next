6afe521c226081e539b9c91d52c2dc9b1f694ea3
satoshi <mimizunobouken@gmail.com>
2022-08-26 17:12:58 +0900

    Aboutコンポーネントにtab機能を追加

--- components/About.js
+++ components/About.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/About.module.css";
 import Title from "./Title";
 import Button from "./Button";
 import Image from "next/image";
+import about from "../libs/about";
 
 const About = () => {
+  const [tab, seTab] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const { desc } = about[value];
+  console.log(desc);
   return (
     <section className={`parts-grid section-margin ${styles.about}`}>
       <div className="section-padding">
         <Title title={"自己紹介"} />
-        <div className={styles.tabcontainer}>
+        <div className={styles.tabcenter}>
           <div>
-            <div className={styles.tabcenter}>
-              <button className={`btn ${styles.tabbtn} ${styles.activebtn}`}>
-                来歴
-              </button>
-              <button className={`btn ${styles.tabbtn}`}>スキル</button>
+            {/* about container */}
+            <div className={styles.tabcontainer}>
+              {about.map((item, index) => {
+                const { title } = item;
+                return (
+                  <>
+                    <button
+                      onClick={() => setValue(index)}
+                      className={`btn ${styles.tabbtn} ${
+                        value === index ? styles.activebtn : ""
+                      }`}
+                    >
+                      {title}
+                    </button>
+                  </>
+                );
+              })}
             </div>
+            {/* about info */}
             <article className={styles.article}>
-              <p>
-                東京を中心とした都市部で、ドローン宅配の実証実験をおこなっています。東京を中心とした都市部で、ドローン宅配の実証実験をおこなっています。東京を中心とした都市部で、ドローン宅配の実証実験をおこなっています。
-              </p>
+              <p>{desc}</p>
               <Button
                 value={{
                   style: styles.aboutbtn,
---
+++ libs/about.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    title: "経歴",
+    desc: "東京を中心とした都市部で、ドローン宅配の実証実験をおこなっています。東京を中心とした都市部で、ドローン宅配の実証実験をおこなっています。東京を中心とした都市部で、ドローン宅配の実証実験をおこなっています。",
+  },
+  {
+    title: "スキル",
+    desc: "I'm baby salvia lomo yuccie, vinyl seitan XOXO tousled bicycle rights slow-carb occupy taiyaki microdosing brunch. Photo booth slow-carb health goth kickstarter cardigan. Roof party pour-over swag pork belly glossier DIY farm-to-table godard locavore. Chicharrones gastropub slow-carb food truck tumblr semiotics, heirloom ugh wolf unicorn thundercats health goth.",
+  },
+];

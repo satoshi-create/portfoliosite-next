dceac30401a8b80e00587c761dde2019bdd0aa07
satoshi <mimizunobouken@gmail.com>
2023-08-07 10:35:28 +0900

    test

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -3,16 +3,18 @@ import { socialLinks } from "../libs//socialLinks";
 import Link from "next/link";
 import styles from "../styles/SocialLinks.module.css";
 
+
 const SocialLinks = ({ value }) => {
   const { styleUl } = value;
   return (
+    
     <ul className={`${styles.links} ${styleUl}`}>
       {socialLinks.map((item, index) => {
         const { name, icon, path } = item;
         return (
           <li key={index}>
             <Link href={path}>
-              <a className={styles.icon}>{icon}</a>
+              <a className={styles.icon}>{icon}test</a>
             </Link>
           </li>
         );

c24bead244fa923a1aa80360dca78b6fac0c5f2f
satoshi-create <mimizunobouken@gmail.com>
2024-02-08 17:09:10 +0900

    ・Footer、LInksコンポーネントを修正

---
+++ .history/components/Links_20240208164814.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { links } from "../libs/links";
+import Link from "next/link";
+import styles from "../styles/Links.module.css";
+
+const Links = () => {
+  return (
+    <ul className={styles.navPc}>
+      
+        </ul>
+
+    </>
+  );
+};
+
+export default Links;
---
+++ .history/components/Links_20240208164815.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { links } from "../libs/links";
+import Link from "next/link";
+import styles from "../styles/Links.module.css";
+
+const Links = () => {
+  return (
+    <ul className={styles.navPc}>
+            {links.map((link, index) => {
+        return (
+          <li key={index}>
+            <Link href={link.path}>
+              <a className={styles.navPcLink}>{link.name}</a>
+            </Link>
+          </li>
+        );
+      })}
+        </ul>
+
+    </>
+  );
+};
+
+export default Links;
